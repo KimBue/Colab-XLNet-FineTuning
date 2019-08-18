aa30576cd47eee0c69bc9e7d627ea639ed84aecc
Kim <kimbuergl@gmail.com>
2019-08-18 13:27:52 +0200

    solving path problem

--- run_classifier.py
+++ run_classifier.py
@@ -338,7 +338,7 @@ class StackoverflowProcessor(DataProcessor):
     def _select_examples(self, data_dir, mode):
         raise NotImplementedError()
     def get_labels(self):
-        return np.zeros(100)
+        return [0 for n in range(100)]
 class StackoverflowBodyProcessor(StackoverflowProcessor):
     def _select_examples(self, data_dir, mode):
         # This code should be optimized
