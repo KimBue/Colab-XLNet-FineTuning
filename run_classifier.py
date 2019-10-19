236aa07eff904ebec9d0e3b1cb89727dbce8a3b7
Kim <kimbuergl@gmail.com>
2019-10-19 19:35:26 +0200

    limit to 10000

--- run_classifier.py
+++ run_classifier.py
@@ -484,11 +484,11 @@ def file_based_convert_examples_to_features(
       f = tf.train.Feature(int64_list=list64)
       return f
 
-    def create_int_feature_alt(values):
+    def create_int_feature(values):
         f = tf.train.Feature(int64_list=tf.train.Int64List(value=list(values)))
         return f
 
-    def create_float_feature(values):
+    def create_float_feature_alt(values):
       if type(values) == type(list()):
         if type(values[0]) == type(list()):
             float_list = tf.train.FloatList(value=values[0])
